2b349c079c31d6662b71a26118027f67c67a6d4f
nicholasturner1 <nturner.stanford@gmail.com>
2017-04-10 14:07:38 -0400

    KL-like optimization and multiple trials

--- src/analysis/sbmopt.jl
+++ src/analysis/sbmopt.jl
@@ -18,7 +18,7 @@ function simpleopt!(sbm::SBM, nepoch=100)
   lls = []
 
   for n in 1:nepoch
-    print("\rEpoch #$n")
+    #print("\rEpoch #$n")
     ll = 0
 
     for i in shuffle(SBMs.getindices(sbm))
@@ -45,7 +45,7 @@ function fasteropt!(sbm::SBM, nepoch=100)
   lls = []
 
   for n in 1:nepoch
-    print("\rEpoch #$n")
+    #print("\rEpoch #$n")
     ll = 0
     ps = SBMs.computeparams(sbm)
 
@@ -104,33 +104,112 @@ function TMopt!(sbm::SBM, nepoch=100)
 end
 
 
-function KLopt!(sbm::SBM, nepoch=100)
-  0#stub
+function KLlikeopt!(sbm::SBM, nepoch=100)
+
+  lls = [];
+
+  for n in 1:nepoch
+    print("\rEpoch #$n")
+
+    ps = SBMs.computeparams(sbm)
+    #keeping two copies so we can update parameters later
+    g  = SBMs.getgroups(sbm)
+    last_g = copy(g)
+
+    epoch_moves = Tuple{Int,Int}[];
+    epoch_lls = Float64[];
+
+    start_ll = SBMs.loglikelihood(sbm,nothing,ps)
+
+    to_move = IntSet(SBMs.getindices(sbm))
+
+    while !isempty(to_move)
+
+      vertex, move, ll = find_best_move_overall(sbm,g,ps, to_move)
+
+      #enacting move for this epoch
+      g[vertex] = move
+      SBMs.updateparams!(sbm,ps,last_g,g)
+      last_g[vertex] = move
+
+      #bookkeeping
+      delete!(to_move, vertex)
+      push!(epoch_moves, (vertex,move))
+      push!(epoch_lls, ll)
+
+    end #while !empty(to_move)
+
+    max_ll, max_i = findmax(epoch_lls)
+
+    if start_ll >= max_ll  break  end
+
+    #replaying changes up to max_ll over the epoch
+    for i in 1:max_i
+      v, cl = epoch_moves[i]
+      SBMs.enactmove(sbm,v,cl)
+      #recording relevant lls
+      push!(lls, epoch_lls[i])
+    end
+
+  end
+
+  lls
 end
 
 
-function multiple_trials(sbm::SBM, opt_fn::Function, num_trials=10, nepochs=100)
+function find_best_move_overall(sbm,g,ps, candidates)
 
-  start_g = SBMs.getgroups(sbm)
+  max_ll = -Inf; max_vertex = 0; max_move = 0;
 
-  best_lls = [] 
+  for i in candidates
+
+    best_move_i, best_ll_i = SBMs.bestmove(sbm,i,g,ps;forcemove=true)
+
+    if best_ll_i > max_ll
+      max_ll = best_ll_i
+      max_vertex = i
+      max_move = best_move_i
+    end
+
+  end
+
+  max_vertex, max_move, max_ll
+end
+
+
+function multiple_trials!(sbm::SBM, opt_fn::Function, num_trials=10, nepochs=100)
+
+  best_lls = []
+  best_groups = []
   final_lls = []
   best_final_ll = -Inf
   final_groups = []
 
   for t in 1:num_trials
 
-    SBMs.setgroups(sbm,start_g)
+    if t == 1
+      print("\rTrial $t")
+    else
+      print("\rTrial $t, Last Trial Time: $(round(toq(),3)) seconds")
+    end
+    tic();
+
+    SBMs.randomize_g!(sbm)
 
     trial_lls = opt_fn(sbm,nepochs)
 
     push!(final_lls, trial_lls[end])
     push!(final_groups, SBMs.getgroups(sbm))
 
-    if trial_lls[end] > best_final_ll  best_lls = trial_lls  end
+    if trial_lls[end] > best_final_ll
+      best_lls = trial_lls
+      best_groups = SBMs.getgroups(sbm)
+    end
   end
 
-  final_lls, final_groups, best_lls
+  toq();
+
+  final_lls, final_groups, best_lls, best_groups
 end
 
 
