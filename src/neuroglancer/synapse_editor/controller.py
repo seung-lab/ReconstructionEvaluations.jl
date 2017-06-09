dd96977c2d996744067b0c554bae0397acfd59f4
Thomas Macrina <tmacrina@princeton.edu>
2017-06-09 16:54:50 -0400

    Update synapse editor app to initial alpha
    * Allow visualization of all synapses, pre or post synapses, and
      allow for display of only synapses shared by loaded segments.
    * Use updated edges class.
    * Allow for simple edge editting. For now, only edge removal.
    * Include hooks for adding edges and undoing changes.

--- src/neuroglancer/synapse_editor/controller.py
+++ src/neuroglancer/synapse_editor/controller.py
@@ -7,11 +7,12 @@ from sockjs.tornado import SockJSConnection, SockJSRouter
 import json
 from collections import OrderedDict
 
+url = "https://neuromancer-seung-import.appspot.com/#!{'layers':{'image':{'type':'image'_'source':'precomputed://gs://neuroglancer/pinky40_v11/image'}_'segmentation':{'type':'segmentation'_'source':'precomputed://gs://neuroglancer/pinky40_v11/watershed_mst_trimmed_sem_remap'_'selectedAlpha':0.19_'equivalences':[['3877668'_'19326814'_'103591140']_['4277677'_'4277679'_'8379193'_'16842140'_'20545530'_'28070152'_'73839508']_['6425103'_'10754651'_'11795110'_'29542973'_'37433948'_'60747980'_'82481626'_'86081566'_'94050687']_['9166702'_'10182838'_'20502779'_'24194138'_'75235878'_'85382145'_'93217930']_['9255584'_'54548179']_['9502487'_'57892747'_'101059677']_['10737881'_'11373304'_'14297219'_'14355397'_'40365597'_'55341024'_'77919835'_'79127657']_['11495691'_'22992948'_'34268137'_'65111487'_'71915931'_'71988027'_'71988205'_'72062252'_'72063123'_'72135023'_'72135238'_'74729756'_'75513595'_'78667963'_'78809798'_'80202998'_'80205054'_'80205200'_'80210970'_'82855759'_'91832299'_'102803839']_['12665626'_'16234274'_'35447257'_'74766084'_'77947793'_'97748394'_'100084599']_['14291369'_'26736864'_'27315159'_'38130284'_'57565958'_'68310210'_'105481088'_'115472755']_['18944199'_'54824643'_'90471147']_['19044008'_'23289708'_'49884185'_'98835405'_'101666914'_'124353669']_['19609439'_'30600998'_'37568546'_'44985981'_'56721274'_'60133104'_'72320295'_'79667216']_['19841583'_'19842233'_'38937946'_'43131056'_'46393266'_'50627772'_'54110205'_'54111199'_'54113377'_'58128408'_'78618103'_'78618508'_'78618671'_'90519274'_'98618299'_'104847879'_'106399811'_'108650349'_'118641208']_['22582085'_'77416179'_'81383260'_'83286327'_'85039013'_'116999114']_['23645010'_'28527929'_'31476008'_'47930143'_'74119606'_'96961145']_['26098483'_'33376321'_'120222283']_['27071013'_'30927698'_'38468916'_'38539982'_'61686594']_['28362126'_'40139878'_'43965341'_'50440972'_'50558763'_'78665518']_['33636469'_'36665115'_'44514762'_'44515403'_'44515711'_'44993562'_'64468455'_'64468662'_'64615913'_'90703146'_'128080543']_['39283678'_'78879706'_'89583008'_'93740872'_'94851047']_['42766729'_'50227332']_['43098463'_'50725796'_'88852094']_['43836373'_'72380820']_['43865569'_'72205144'_'72828887'_'83631509'_'107876121']_['44140753'_'56607057'_'60687267'_'67920322'_'68205767'_'71749147'_'79139587'_'102854064'_'106664882'_'131506448']_['47936209'_'101023196']_['49727582'_'53136623'_'60420282'_'69938647'_'69944797'_'73870846'_'82007157'_'85531235'_'85739518'_'89344280'_'89414904'_'97141883'_'103741064'_'112034363'_'115377257'_'115413299'_'122910353'_'123316247'_'126527785']_['50996603'_'54996023'_'56179869'_'59016628'_'107860456']_['56627771'_'56682259'_'68830633'_'71743754'_'72646749'_'76434188'_'86482605'_'90749372'_'123211126']_['58045989'_'82937248'_'118730806']_['58237135'_'81728319'_'88764136'_'96468213']_['59520759'_'115244269'_'127126631'_'131011965']_['65727663'_'113907310']_['65959493'_'100323945'_'104528748'_'115696692'_'122808597']_['85733207'_'88948820'_'122609726']_['94563039'_'127566507']]}_'synapses':{'type':'point'_'points':[]}_'psds':{'type':'segmentation'_'source':'precomputed://s3://neuroglancer/pinky40_v11/psdsegs_mst_trimmed_sem'_'visible':false}}_'navigation':{'pose':{'position':{'voxelSize':[4_4_40]_'voxelCoordinates':[22469_34463_726]}}_'zoomFactor':5.497982928501263}_'layout':'xy-3d'_'perspectiveZoom':1230.0703725144326_'perspectiveOrientation':[-0.8363621234893799_-0.12134983390569687_0.5306068062782288_-0.06503099203109741]_'showSlices':false_'stateURL':'https://localhost:7777'_'showAxisLines':false}"
+print(url)
+
 # need to run controller from ReconstructionEvaluations/src/neuroglancer
 sys.path.append("../neuroglancer")
 from model import Model
-from ui import UI
-import edges
 
 clients = set()
 n_messages = 0
@@ -39,26 +40,28 @@ class Controller:
         and their classifications
     """
     def __init__(self):
-        self.synapses = []
-        self.segments = []
         self.synapses_on = True
-        self.segments_on = True
+        self.neighbors_on = True
         self.shared_synapses_only = True
         self.shared_segments_only = True
 
+    def update_segment_display(self):
+        segs = model.get_segments(self.neighbors_on)
+        current_state['layers']['segmentation']['segments'] = segs
+        broadcast()
+
+    def update_synapse_display(self):
+        coords = model.get_coords() if self.synapses_on else []
+        current_state['layers']['synapses'] = {'type':'point', \
+                                                        'points':coords}
+        broadcast()
+
     def update_display(self):
-        if self.segments_on:
-            segs = model.get_segments()
-            current_state['layers']['segmentation']['segments'] = segs
-        else:
-            current_state['layers']['segmentation']['segments'] = []
-        if self.synapses_on:
-            coords = model.get_synapses()
-            current_state['layers']['synapses'] = {'type':'synapse', \
-                                                            'points':coords}
-        else:
-            current_state['layers']['synapses'] = {'type':'synapse', \
-                                                            'points':[]}
+        segs = model.get_segments(self.neighbors_on)
+        current_state['layers']['segmentation']['segments'] = segs
+        coords = model.get_coords() if self.synapses_on else []
+        current_state['layers']['synapses'] = {'type':'point', \
+                                                        'points':coords}
         broadcast()
 
     def get_segments(self):
@@ -79,20 +82,21 @@ class Controller:
         coords = self.get_synapses()
         model.update_synapses(coords)
 
-    def toggle_segments(self):
-        """Toggle whether segments are displayed or not
+    def toggle_neighbors(self):
+        """Toggle whether neighbors are displayed or not
         """
-        self.segments_on = 0 if self.segments_on else 1
-        return self.update_display()
+        self.neighbors_on = not self.neighbors_on
+        return self.update_segment_display()
 
     def toggle_synapses(self):
-        """Toggle whether segments are displayed or not
+        """Toggle whether synapses are displayed or not
         """
-        self.synapses_on = 0 if self.synapses_on else 1
-        return self.update_display()        
+        self.synapses_on = not self.synapses_on
+        return self.update_synapse_display()        
 
     def save(self):
-        model.write()
+        self.update_synapses()
+        model.save()
 
     def get_synapses(self):
         coords = None
@@ -122,7 +126,6 @@ class Connection(SockJSConnection):
         if not n_messages:
             print('state initialized')
         n_messages += 1
-        c.check_ng()
 
     def on_close(self):
         # If client disconnects, remove him from the clients list
@@ -155,7 +158,7 @@ class TornadoThread(threading.Thread):
             "certfile": "./certificate.crt",
             "keyfile": "./privateKey.key",
         })
-        http_server.bind(9999) #port
+        http_server.bind(7777) #port
         http_server.start(1)
 
     def run(self):
--- src/neuroglancer/synapse_editor/model.py
+++ src/neuroglancer/synapse_editor/model.py
@@ -1,34 +1,46 @@
-import edges
+from edges import Edges
 import copy
 import h5py
 import numpy as np
 
 class Segment(object):
 
-    def __init__(self, edge_dict, seg_id=0):
+    def __init__(self, edges, seg_id=0):
         """Segment object loads relevant information for specifc segment
         """
-        self.edge_dict = edge_dict
-        self.seg_id = seg_id
+        self.id = seg_id
     
-        self.pre_neighbors
-        self.post_neighbors
-        self.pre_synapses
-        self.post_synapses
+        self.pre_neighbors = edges.post_to_pre.get(seg_id, [])
+        self.post_neighbors = edges.pre_to_post.get(seg_id, [])
+        self.pre_synapses = edges.pre_to_syns.get(seg_id, [])
+        self.post_synapses = edges.post_to_syns.get(seg_id, [])
+        print(self)
 
-        self.neighbors = edge_dict['seg_to_neighbors'][seg_id]
-        self.synapses = edge_dict['seg_to_syn'][seg_id]
+    def get_synapses(self, type='all'):
+        synapses = self.pre_synapses + self.post_synapses
+        if type == 'pre':
+            synapses = self.pre_synapses
+        elif type == 'post':
+            synapses = self.post_synapses
+        return synapses
 
-    def get_synapses(self):
-        return self.synapses
-
-    def get_neighbors(self):
-        return self.neighbors
+    def get_neighbors(self, type='all'):
+        segments = self.pre_neighbors + self.post_neighbors
+        if type == 'pre':
+            segments = self.pre_neighbors
+        elif type == 'post':
+            segments = self.post_neighbors
+        return segments
 
     def __repr__(self):
-        s  = "seg id:\t" + str(self.seg_id) + "\n"
-        s += "neighbors:\t" + str(len(self.neighbors)) + "\n"
-        s += "synapses:\t" + str(len(self.synapses)) + "\n"
+        s  = "seg id:\t" + str(self.id) + "\n"
+        s += "neighbors:\t" + str(len(self.get_neighbors())) + "\n"
+        s += "\tpre:\t" + str(len(self.pre_neighbors)) + "\n"
+        s += "\tpost:\t" + str(len(self.post_neighbors)) + "\n"
+        s += "synapses:\t" + str(len(self.get_synapses())) + "\n"
+        s += "\tpre:\t" + str(len(self.pre_synapses)) + "\n"
+        s += "\tpost:\t" + str(len(self.post_synapses)) + "\n"
+        return s
 
 class Model(object):
 
@@ -45,63 +57,81 @@ class Model(object):
             current_segment: ID of segment being viewed
             segment_idx: index of current segment
         """
-        self.edge_dict = {}
+        self.edges = Edges(src_file, dst_file)
         self.original_edge_dict = {}
         self.synapses = []
         self.coords = []
         self.segments = []
         self.neighbors = []
-        self.src_file = src_file
-        self.dst_file = dst_file
-
-        self.load(src_file)
+        self.type = 'all'
+        self.shared = False
 
     def set_segments(self, seg_ids):
-        self.segments = [Segment(seg_id) for seg_id in seg_ids]
+        self.segments = [Segment(self.edges, seg_id) for seg_id in seg_ids]
+        self.load_synapses()
+        self.load_neighbors()
+        print(self)
 
-    def load_all_synapses(self):
-
-    def load_shared_synapses(self):
-
-    def load_pre_synapses(self):
-
-    def load_post_synapses(self):
+    def load_synapses(self):
+        synapses = []
+        for seg in self.segments:
+            synapses.append(set(seg.get_synapses(self.type)))
+        if self.shared:
+            shared_synapses = []
+            for i, syn_i in enumerate(synapses):
+                for j, syn_j in enumerate(synapses):
+                    if i != j:
+                        shared_synapses.append(syn_i.intersection(syn_j))
+            synapses = shared_synapses
+        self.synapses = sum(map(list, synapses), [])
+        self.load_synapse_centroids()
 
     def load_synapse_centroids(self):
+        coords = []
+        for syn in self.synapses:
+            coords.append(self.edges.syn_coords[syn])
+        self.coords = coords
 
     def load_pre_post_centroids(self):
+        coords = []
+        for syn in self.synapses:
+            coords.append(self.edges.syn_coords_pre_post[syn])
+        self.coords = coords
 
-    def load_all_neighbors(self):
-
-    def load_shared_neighbors(self):
-
-    def load_pre_neighbors(self):
-
-    def load_post_neighbors(self):
+    def load_neighbors(self):
+        neighbors = []
+        for seg in self.segments:
+            neighbors.append(set(seg.get_neighbors(self.type)))
+        if self.shared:
+            shared_neighbors = []
+            for i, seg_i in enumerate(neighbors):
+                for j, seg_j in enumerate(neighbors):
+                    if i != j:
+                        shared_neighbors.append(seg_i.intersection(seg_j))
+            neighbors = shared_neighbors
+        self.neighbors = sum(map(list, neighbors), [])
 
     def get_synapses(self):
+        return self.synapses
 
-    def get_segments(self):
+    def get_coords(self):
+        return self.coords
+
+    def get_segments(self, neighbors=False):
+        segs = [seg.id for seg in self.segments]
+        segs += self.neighbors if neighbors else []
+        return segs
 
     def get_neighbors(self):
+        return self.neighbors
 
-    def reset(self):
+    def undo(self):
+        self.edges.undo()
 
-    def update_synapses(self, coords):
-        edges.add_edge
-        edges.remove_edge
-
-    def load(self, filename):
-        """Reads data from csv file, filename, and processes it into dict
-        """
-        self.edge_dict = edges.create_edge_dict(edges.load_edges(filename))
-        self.original_edge_dict = copy.deepcopy(self.edge_dict)
-
-    def write(self):
+    def save(self):
         """Write update edges csv to outfile
         """
-        print 'Writing edges to ' + self.outfile
-        edges.write_dicts_to_edges(self.outfile, self.edge_dict)
+        self.edges.save()
 
     def get_seg_value(self, coord):
         """Lookup coordinate in the segmentation H5 file
@@ -110,8 +140,8 @@ class Model(object):
         dset = f['/main']
         return int(dset[tuple(coord)])
 
-    def update_dicts(self, current_synapse_coordinates):
-        """Update the edge dicts based on the list of working synapse coords
+    def update_synapses(self, new_coords, pre_post_centroids=False):
+        """Update the edge dicts based on the list of new synapse coords
 
         Run through current coords and the working list, tabulating which need
         to be removed from the current list, and what needs to be added from
@@ -120,62 +150,49 @@ class Model(object):
         Then run through the "to add" and "to remove" lists, making the 
         adjustments to the edge dicts.
         """
-        coords = current_synapse_coordinates
-        temp_coords = zip(coords[::2], coords[1::2])
-        coords = self.synapse_coordinates
-        cur_coords = zip(coords[::2], coords[1::2])
+        old_coords = self.coords
 
         syn_to_remove = np.ones(len(self.synapses), dtype=bool)
-        syn_to_add = np.ones(len(temp_coords), dtype=bool)
-        for i, (cur_pre, cur_post) in enumerate(cur_coords):
-            for j, (temp_pre, temp_post) in enumerate(temp_coords):
-                if (temp_pre == cur_pre) & (temp_post == cur_post):
+        syn_to_add = np.ones(len(new_coords), dtype=bool)
+
+        for i, old_coord in enumerate(old_coords):
+            for j, new_coord in enumerate(new_coords):
+                if old_coord == new_coord:
                     syn_to_remove[i] = False
                     syn_to_add[j] = False
 
         for syn in np.array(self.synapses)[syn_to_remove]:
-            print 'deleting synapse no. ' + str(syn)
-            pre, post = self.edge_dict['syn_to_segs'][syn]
-            del self.edge_dict['syn_to_segs'][syn]
-            del self.edge_dict['syn_coords'][syn]
-            del self.edge_dict['syn_size'][syn]
-            del self.edge_dict['syn_coords_pre_post'][syn]
-            self.edge_dict['seg_to_syn'][pre].remove(syn)
-            self.edge_dict['seg_to_syn'][post].remove(syn)
-            shared_synapses = self.edge_dict['segs_to_syn'][(pre, post)]
-            if len(shared_synapses) > 1:
-                self.edge_dict['segs_to_syn'][(pre, post)].remove(syn)
-            else:
-                del self.edge_dict['segs_to_syn'][(pre, post)]
-                self.edge_dict['pre_to_post'][pre].remove(post)
-                self.edge_dict['post_to_pre'][post].remove(pre)
+            self.edges.remove_edge(syn)
 
-        for pre_coord, post_coord in np.array(temp_coords)[syn_to_add].tolist():
-            pre = self.read_segment_id(reversed(pre_coord))
-            post = self.read_segment_id(reversed(post_coord))
-            print 'Proposed pre: ' + str(pre) + ':' + str(pre_coord)
-            print 'Proposed post: ' + str(post) + ':' + str(post_coord)
-            if (pre == self.current_segment) | (post == self.current_segment):
-                self.max_syn_id += 1
-                s = self.max_syn_id
-                print 'adding synapse no. ' + str(s)
-                points = np.array([pre_coord, post_coord])
-                center = np.round(np.mean(points, axis=0)).astype(int).tolist()
-                self.edge_dict['syn_to_segs'][s] = [pre, post]
-                self.edge_dict['syn_coords'][s] = center
-                self.edge_dict['syn_size'][s] = 0
-                self.edge_dict['syn_coords_pre_post'][s] = [pre_coord, post_coord]
-                self.edge_dict['segs_to_syn'][(pre, post)] = s
-                edges.push_dict(self.edge_dict['seg_to_syn'], pre, s)
-                edges.push_dict(self.edge_dict['seg_to_syn'], post, s)
-                edges.push_dict(self.edge_dict['pre_to_post'], pre, post)
-                edges.push_dict(self.edge_dict['post_to_pre'], post, pre)
-            else:
-                print 'Trying to add synapse at ' + str(pre_coord) + ',' + \
-                        str(post_coord) + ', but not part of segment ' + \
-                        str(self.current_segment) + '. Move synapse or ' + \
-                        'switch to appropriate segments.'
+        # if pre_post_centroids:
+        #     new_coords = zip(new_coords[::2], new_coords[1::2])
+        #     old_coords = zip(self.coords[::2], self.coords[1::2])
 
-        edges.unique(self.edge_dict['pre_to_post'])
-        edges.unique(self.edge_dict['post_to_pre'])
-        self.load_synapses()
+        # for i, (old_pre, old_post) in enumerate(old_coords):
+        #     for j, (new_pre, new_post) in enumerate(new_coords):
+        #         if (new_pre == old_pre) & (new_post == old_post):
+        #             syn_to_remove[i] = False
+        #             syn_to_add[j] = False
+
+        # for coord in np.array(new_coords)[syn_to_add].tolist():
+        #     if pre_post_centroids:
+        #         pre_coord, post_coord = coord
+        #     else:
+        #         pre_coord, post_coord = coord, coord
+        #     pre = self.get_seg_value(reversed(pre_coord))
+        #     post = self.get_seg_value(reversed(post_coord))
+        #     if pre != 0 and post != 0:
+        #         self.edges.add_edge(pre, post, pre_coord, post_coord)
+
+    def __repr__(self):
+        s  = 'no\tsyn\tcentroid\t\tpre\t\tpost\n'
+        for i, (syn, coord) in enumerate(zip(self.synapses, self.coords)):
+            pre = self.edges.syn_to_pre_seg[syn]
+            post = self.edges.syn_to_post_seg[syn]
+            s += str(i) + '\t' 
+            s += str(syn) + '\t' 
+            s += str(coord) + '\t'
+            s += str(pre) + '\t'
+            s += str(post) + '\t'
+            s += '\n'
+        return s
